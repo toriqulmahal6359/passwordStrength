2f63ee5ca6097d7ab1d6a32434bc531a595ac251
toriqul <toriqulmahal6359@yahoo.com>
2024-04-16 14:45:27 +0600

    Initial commit

---
+++ PasswordStrengthApplication/Register.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="PasswordStrengthApplication.Register" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+    <script>
+        function checkPasswordStrength() {
+            var password = document.getElementById('<%= txtPassword.ClientID %>').value;
+            // Your JavaScript code to check password strength and provide feedback to users
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtPassword" runat="server" TextMode="Password" onchange="checkPasswordStrength()"></asp:TextBox>
+            <asp:Label ID="lblPasswordStrength" runat="server" Text=""></asp:Label>
+            <asp:Button ID="btnRegister" runat="server" Text="Register" OnClick="btnRegister_Click" />
+            <asp:Label ID="lblErrorMessage" runat="server" ForeColor="Red" Visible="false"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ PasswordStrengthApplication/Register.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace PasswordStrengthApplication
+{
+    public partial class Register : System.Web.UI.Page
+    {
+
+        //     protected void Page_Load(object sender, EventArgs e) {}
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+            {
+                string password = txtPassword.Text;
+
+                // Perform server-side validation
+                if (!IsStrongPassword(password))
+                {
+                    lblErrorMessage.Text = "Password must meet complexity requirements.";
+                    lblErrorMessage.Visible = true;
+                    return;
+                }
+
+                // Registration logic here
+            }
+
+            private bool IsStrongPassword(string password)
+            {
+                // Password complexity requirements
+                int minLength = 8;
+                int minDigitCount = 1;
+                int minUpperCaseCount = 1;
+                int minLowerCaseCount = 1;
+
+                // Check password length
+                if (password.Length < minLength)
+                    return false;
+
+                // Check for at least one digit
+                if (password.Count(char.IsDigit) < minDigitCount)
+                    return false;
+
+                // Check for at least one uppercase letter
+                if (password.Count(char.IsUpper) < minUpperCaseCount)
+                    return false;
+
+                // Check for at least one lowercase letter
+                if (password.Count(char.IsLower) < minLowerCaseCount)
+                    return false;
+
+                // Additional complexity checks can be added here
+                // For example, checking for special characters
+
+                return true;
+            }
+    }
+ }
